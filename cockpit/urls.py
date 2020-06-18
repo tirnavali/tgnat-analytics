2662d895ae021ca707abb8071a36a99ed4a859ac
ss90 <tran.ce.co@gmail.com>
2020-06-18 12:22:20 +0300

    referans modeli için veri düzenleme tamamlandı

--- cockpit/urls.py
+++ cockpit/urls.py
@@ -2,9 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('selamla/', views.GreetingView.as_view()),
     path('', views.MarketingPage.as_view(), name='marketing_page'),
-
     path('referans/', views.index, name='referance'),        
     path('referans/yeni/', views.RefAnalyticsFormView.as_view(), name='new_record_ref'),
     path('referans/<int:pk>/', views.RefAnalyticsDetailView.as_view(), name="detail"),
--- cockpit/views.py
+++ cockpit/views.py
@@ -19,47 +19,93 @@ class GroupViewSet(viewsets.ModelViewSet):
     serializer_class = GroupSerializer
 
 def index(request):
+    """ 
+    ReferenceServiceAnalytic modelinin index sayfası
+    """
     latest_data_list = ReferenceServiceAnalytic.objects.order_by('-record_date')
-    context = {'latest_data_list': latest_data_list}
+    context = {'latest_data_list': latest_data_list,
+    'modul_baslik'  :"Referans Hizmetleri Analitikleri"}
     return render(request, 'cockpit/index.html', context)
 
-def detail(request, analytic_id):
+""" def detail(request, analytic_id):
     return HttpResponse("You're on the analytics detail page %s." % analytic_id)
 
 def new_record_ref(request):
     form = ReferenceAnalyticForm()
-    return render(request, 'cockpit/new_record_ref.html', locals())
+    return render(request, 'cockpit/new_record_ref.html', locals()) """
 
-class GreetingView(View):
-    greeting = "Good day"
-
-    def get(self, request):
-        return HttpResponse(self.greeting)
 
 class MarketingPage(View):
+    """
+    Web sitesine açılışta görünecek olan ilk sayfayı yüklemek ile sorumludur.
+    """
     baslik = "Hoşgeldiniz"
+    
 
     def get(self, request):
         return render(request, 'cockpit/marketing.html', locals())
 
 
 class RefAnalyticsFormView(View):
+    """
+    ReferenceServiceAnalytic modeli için;
+    GET, POST, PUT, DELETE görevlerini yerine getirir.
+    """
     form = ReferenceAnalyticForm()
     template_name = 'cockpit/new_record_ref.html'
+    
+    obj = ReferenceServiceAnalytic()
 
-    def get(self, request, pk):
+    def get(self, request, *args, **kwargs):
+        """
+        Bu fonksiyona hem düzenleme hemde yeni veri sayfası görüntüleme isteği gelebilir.
+        Eğer düzenleme sayfası isteği varsa pk dolu olmalıdır.
+        Aksi halde yeni gönderi sayfası talep edildiği anlaşılacaktır.
+        """
+        pk = self.kwargs.get('pk')
         if pk:
+            try:
+                self.obj = ReferenceServiceAnalytic.objects.get(pk=pk)
+                initial = { 'user_from_out' : self.obj.user_from_out,
+                            'user_from_inside' :self.obj.user_from_inside,
+                            'online_user_outside': self.obj.online_user_outside,
+                            'online_user_inside': self.obj.online_user_inside,
+                            'borrowed_books': self.obj.borrowed_books,
+                            'retired_books': self.obj.retired_books,
+                            'photocopy': self.obj.photocopy,
+                            'record_date': self.obj.record_date}
+                form = ReferenceAnalyticForm(initial = initial)
+                return render(request, self.template_name, {'form': form})
+            except:
+                return HttpResponse("Böyle bir sayfa yok")
+
             print("NESNEYİ BUL VE FORMA BAS")
             print("PK var {}".format(str(pk)))
-        form = self.form
-        return render(request, self.template_name, {'form': form})
+        else:
+            form = self.form
+            return render(request, self.template_name, {'form': form})
 
-    def post(self, request):
+    def post(self, request, *args, **kwargs):
+        """
+        Bu fonksiyona hem düzenleme hem de yeni veri kaydetme isteği gelebilir.
+        Eğer düzenleme isteği varsa pk dolu olmalıdır.
+        Aksi halde yeni gönderi olduğu anlaşılacaktır.
+        """
+        pk = self.kwargs.get('pk')
         form = ReferenceAnalyticForm(request.POST)
         form_errors = []
+        model_data = ReferenceServiceAnalytic()
+
+        # eğer düzenleme isteği gelirse düzenlenecek nesneyi bul
+        if pk:
+            try:
+                model_data = ReferenceServiceAnalytic.objects.get(pk=pk)
+            except:
+                return HttpResponse("Böyle bir düzenlenme isteği uygun değil")
+        
+        # normal isteklerde burada devam et
+        # form geçerliyse bu işlemleri yap
         if form.is_valid():
-    
-            model_data = ReferenceServiceAnalytic()
             model_data.user_from_out = request.POST.get('user_from_out')
             model_data.user_from_inside = request.POST.get('user_from_inside')
             model_data.online_user_outside = request.POST.get('online_user_outside')
@@ -68,14 +114,18 @@ class RefAnalyticsFormView(View):
             model_data.retired_books = request.POST.get('retired_books')
             model_data.photocopy = request.POST.get('photocopy')
             model_data.record_date = request.POST.get('record_date')
-
+        
+        # forma eksi değer girilemez kontrol
             if model_data.is_minus_value_entered():
                 form_errors.append("Forma eksi (-) değer girilemez.")
                 return render(request, self.template_name, {'form': form, 'errors' : form_errors})
             else:
                 model_data.save()
-                return redirect('index')
+                return redirect('detail', pk= model_data.pk)
         return render(request, self.template_name, {'form': form})
 
 class RefAnalyticsDetailView(DetailView):
+    """
+    ReferenceServiceAnalytic modeli için ayrıntılar sayfası oluşturur.
+    """
     model = ReferenceServiceAnalytic
